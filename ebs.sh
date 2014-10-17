e1f746090f4d56ae898ff2d014d59e62f5579603
EC2 Default User <ec2-user@ip-10-63-23-196.eu-west-1.compute.internal>
2014-10-17 18:38:57 +0000

    release of ebs.sh: it checks to see if there is an fstab entry and works around it

--- ebs.sh
+++ ebs.sh
@@ -1,29 +1,61 @@
 #!/bin/bash
 
-yum update -y
-yum upgrade -y
+#lists all unmounted volumes
+volumes=""
+for vol in `lsblk|grep -v NAME | awk '{ print $1 }'`
+do 
+    vol=`echo $vol | sed 's/└─//g'`
+    if [[ $(mount | grep -c  $vol) = 0 ]]
+    then
+        volumes=$volumes" "$vol
+    fi
+done
 
-lsblk
-echo "enter block device: "
-read BLOCK
-
-echo "do you need to build a file system for the volume?"
-read -p "please note this will wipe all data on the volume (y/n) " fileBuild
-
-if [ $fileBuild = "y" ]
+#if there are any volumes, this will display them and ask if the user wants to mount any
+if [[ $(echo $volumes | wc -m) > 1 ]] 
 then
-    file -s /dev/$BLOCK
-    mkfs -t ext4 /dev/$BLOCK
+    lsblk | grep NAME
+    for i in $volumes
+    do
+        lsblk | grep $i
+    done
+    read -p "would you like to mount these now? (y/n) " volmount
 fi
 
-echo "enter location to mount directory"
-read MOUNT
-echo "mkdir $MOUNT"
 
-mount /dev/$BLOCK $MOUNT 
 
-cp /etc/fstab /etc/fstab.orig
+fileBuild=y
 
-fstabEntry="/dev/$BLOCK $MOUNT ext4 defaults,noatime 0 2"
-echo $fstabEntry >> /etc/fstab
-mount -a
+if [[ $volmount = "y" ]]
+then
+    for ((i=1; i<=$(echo $volumes | wc -w); i++)); do
+        read -p "enter block device: " BLOCK
+        read -p "enter location to mount directory: " MOUNT
+
+        #checks to see if volumes are in fstab, if they are it asks the user if they want to remove them
+        #FUTURE: will show volumes and compare them with what user has specified and ask if user wants to replace them
+        #FUTURE: will check to see if mount points are taken and will ask if user wants to remove/replace them
+        entry=$(grep $MOUNT /etc/fstab)
+        if [[ $(echo $entry | wc -m) > 1 ]]
+        then
+            read -p "There is an fstab entry for $MOUNT, do you want to delete it and re-enter it? (y/n): " fileBuild
+        fi
+
+        #backs up fstab
+        cp /etc/fstab /etc/fstab.orig
+        #if user wants to replace fstab values, this is done here
+        if [[ $fileBuild = "y" ]]
+        then
+            echo $volumes | xargs -n1 -I {} sed -i '/.*'"{}"'.*/d' /etc/fstab
+            read -p "rebuilding file system, this will wipe all data on drive, press any key to contine (if you can't find the any key I suggest purchasing a mac)"
+            mkfs -t ext4 /dev/$BLOCK
+            fstabEntry="/dev/$BLOCK $MOUNT ext4 defaults,noatime 0 2"
+            echo $fstabEntry >> /etc/fstab
+        fi
+
+        echo $BLOCK
+        file -s /dev/$BLOCK
+        mount /dev/$BLOCK $MOUNT 
+        mount -a
+    done
+fi
