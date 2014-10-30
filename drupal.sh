bba73e554a01a164cbf986edbba0dd3a0faade91
EC2 Default User <ec2-user@ip-10-63-23-196.eu-west-1.compute.internal>
2014-10-30 08:03:32 +0000

    fixing database connection bug

--- drupal.sh
+++ drupal.sh
@@ -22,21 +22,26 @@ aws configure
 aws rds describe-db-instances | egrep "DBName|Address|MasterUsername" | sed 's/"//g'
 echo "above you see the various databases in the default region, please select which RDS database you would like to use accourding to the main database name. You will get a change to change this later if you like."
 read DBName
-echo "thank you, you have selected the " $DBName " would you like to use a different database? (y/n)"
-read diffName
 
-if [ $diffName = "y" ]
-then
-    echo "please enter the name of the database you would like to create:"
-    read DBName
-fi
+read -p "please enter the name of the database you wish to create" tableName
+#echo "thank you, you have selected the " $DBName " would you like to use a different database? (y/n)"
+#read diffName
 
+#if [ $diffName = "y" ]
+#then
+#    echo "please enter the name of the database you would like to create:"
+#    read DBName
+#fi
+
+echo "DBName is $DBName"
 DNS=$(aws rds describe-db-instances --db-instance-identifier $DBName | egrep "Address" | sed 's/.*|  //;s/ .*//')
+echo "DNS is $DNS"
 username=$(aws rds describe-db-instances --db-instance-identifier $DBName | egrep "MasterUsername" | sed 's/.*|  //;s/ .*//')
+echo "username is $username"
 
 mysql -h $DNS -P 3306 -u $username -p << EOF
-DROP DATABASE $DBName;
-CREATE DATABASE $DBName;
+DROP DATABASE $tableName;
+CREATE DATABASE $tableName;
 DELETE FROM mysql.user WHERE user = ''; 
 FLUSH PRIVILEGES; 
 EOF
