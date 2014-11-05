4385d2880c37d5dd2d2ac554629778ea236c2b72
EC2 Default User <ec2-user@ip-10-63-23-196.eu-west-1.compute.internal>
2014-11-05 09:13:12 +0000

    intoducing postgres

--- databaseSetup.sh
+++ databaseSetup.sh
@@ -25,24 +25,32 @@ EOF
     read -p "otherwise exit this script and troubleshoot"
 }
 
-postgres() {echo "no actions required for postgres"}
+postgres() {
+    echo "no actions required for postgres"
+}
 
-echo "you are now going to configure your AWS tools, please have your access keys and secret access keys to hand. It is reccomended that this be an IAM access key, this will make your life easier in the event of a compromise."
+choosedb() {
+    echo "you are now going to configure your AWS tools, please have your access keys and secret access keys to hand. It is reccomended that this be an IAM access key, this will make your life easier in the event of a compromise."
 
-aws configure
+    aws configure
+
+
+    aws rds describe-db-instances | egrep "DBInstanceIdentifier|Address|MasterUsername" | sed 's/"//g'
+    echo "above you see the various databases in the default region, please select which RDS database you would like to use accourding to the main database name. You will get a change to change this later if you like."
+    read DBName
+
+    DNS=$(aws rds describe-db-instances --db-instance-identifier $DBName | egrep "Address" | sed 's/.*|  //;s/ .*//')
+    username=$(aws rds describe-db-instances --db-instance-identifier $DBName | egrep "MasterUsername" | sed 's/.*|  //;s/ .*//')
+}
+
 
 echo "mysql (1)"
+echo "postgres (2)"
 read -p "please enter the number of the database you wish to use: " choice 
-
-aws rds describe-db-instances | egrep "DBInstanceIdentifier|Address|MasterUsername" | sed 's/"//g'
-echo "above you see the various databases in the default region, please select which RDS database you would like to use accourding to the main database name. You will get a change to change this later if you like."
-read DBName
-
-DNS=$(aws rds describe-db-instances --db-instance-identifier $DBName | egrep "Address" | sed 's/.*|  //;s/ .*//')
-username=$(aws rds describe-db-instances --db-instance-identifier $DBName | egrep "MasterUsername" | sed 's/.*|  //;s/ .*//')
 
 case "$choice" in
     1)
+        choosedb
         mysqlSetup
         ;;
 
